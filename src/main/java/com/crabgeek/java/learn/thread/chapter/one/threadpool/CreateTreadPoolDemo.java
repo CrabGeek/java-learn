88757ee99320d311b109da2c57906c189eebd744
Guipengtt <xgp980303@gmail.com>
2022-12-25 22:11:12 +0800

    Add method for execute and submit

--- src/main/java/com/crabgeek/java/learn/thread/chapter/one/threadpool/CreateTreadPoolDemo.java
+++ src/main/java/com/crabgeek/java/learn/thread/chapter/one/threadpool/CreateTreadPoolDemo.java
@@ -1,8 +1,10 @@
 package com.crabgeek.java.learn.thread.chapter.one.threadpool;
 
 import lombok.SneakyThrows;
+import org.apache.commons.lang3.RandomUtils;
 import org.junit.Test;
 
+import java.util.Random;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -12,7 +14,7 @@ public class CreateTreadPoolDemo {
     static class TargetTask implements Runnable {
 
         static AtomicInteger taskNo = new AtomicInteger(1);
-        private String taskName;
+        protected String taskName;
 
         public TargetTask() {
             taskName = "task-" + taskNo.get();
@@ -25,6 +27,14 @@ public class CreateTreadPoolDemo {
             System.out.println("任务: " + taskName + " doing");
             Thread.sleep(SLEEP_GAP);
             System.out.println(taskName + ": 运行结束");
+        }
+    }
+
+    static class TargetTaskWithError extends TargetTask {
+        @Override
+        public void run() {
+            super.run();
+            throw new RuntimeException("Error from " + this.taskName);
         }
     }
 
@@ -88,6 +98,41 @@ public class CreateTreadPoolDemo {
             pool.execute(new TargetTask());
         }
         Thread.sleep(10000);
+        pool.shutdown();
+    }
+
+    @SneakyThrows
+    @Test
+    public void testSubmit() {
+        ExecutorService pool = Executors.newFixedThreadPool(2);
+        Future<Integer> future = pool.submit(() -> RandomUtils.nextInt(200, 300));
+
+        try {
+            Integer result = future.get();
+            System.out.println("异步执行的结果: " + result);
+        } catch (ExecutionException e) {
+            System.out.println("异步调用被中断");
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            System.out.println("异步调用过程中，发生了异常");
+            e.printStackTrace();
+        }
+        pool.shutdown();
+    }
+
+    @Test
+    public void testSubmit2() {
+        ExecutorService pool = Executors.newFixedThreadPool(2);
+        Future<?> future = pool.submit(new TargetTaskWithError());
+
+        try {
+            if (future.get() == null) {
+                System.out.println("执行完成");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
         pool.shutdown();
     }
 }
